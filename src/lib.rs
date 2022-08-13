433025bcb6536501b676f46746c81f7d0c565bd1
SmallNibbler <smallnibbler@gmail.com>
2022-08-13 22:03:57 +0300

    Remove audio system

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod actions;
-mod audio;
 mod loading;
 mod menu;
 mod player;
 
 use crate::actions::ActionsPlugin;
-use crate::audio::InternalAudioPlugin;
 use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
 use crate::player::PlayerPlugin;
@@ -36,7 +34,6 @@ impl Plugin for GamePlugin {
             .add_plugin(LoadingPlugin)
             .add_plugin(MenuPlugin)
             .add_plugin(ActionsPlugin)
-            .add_plugin(InternalAudioPlugin)
             .add_plugin(PlayerPlugin);
 
         #[cfg(debug_assertions)]
--- src/loading.rs
+++ src/loading.rs
@@ -1,7 +1,6 @@
 use crate::GameState;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use bevy_kira_audio::AudioSource;
 
 pub struct LoadingPlugin;
 
@@ -13,7 +12,6 @@ impl Plugin for LoadingPlugin {
         app.add_loading_state(
             LoadingState::new(GameState::Loading)
                 .with_collection::<FontAssets>()
-                .with_collection::<AudioAssets>()
                 .with_collection::<TextureAssets>()
                 .continue_to_state(GameState::Menu),
         );
@@ -27,12 +25,6 @@ impl Plugin for LoadingPlugin {
 pub struct FontAssets {
     #[asset(path = "fonts/FiraSans-Bold.ttf")]
     pub fira_sans: Handle<Font>,
-}
-
-#[derive(AssetCollection)]
-pub struct AudioAssets {
-    #[asset(path = "audio/flying.ogg")]
-    pub flying: Handle<AudioSource>,
 }
 
 #[derive(AssetCollection)]
